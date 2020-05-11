e082d6eb9541797dac73794a166afa9ffcf913fa
Hanno <h.hildenbrandt@rug.nl>
2020-05-11 14:57:16 +0200

    Rcpp::compileAttributes added

--- build_packages.sh
+++ build_packages.sh
@@ -4,6 +4,11 @@
 # attemps to build all remphasis source packages it can find
 # Hanno 2020
 
+cd remphasis && \
+rm -f ./src/RcppExports.*
+Rscript -e 'library(Rcpp); compileAttributes(".")' && \
+cd ..
+
 for i in $(ls -d remphasis*/); do 
-    R CMD build ${i%%/}; 
+    R CMD build ${i%%} 
 done
